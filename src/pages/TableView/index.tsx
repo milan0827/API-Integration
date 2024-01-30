915114f1fb5799165511666630942bdd554712fe
milan0827 <milanrokaya2014@gmail.com>
2024-01-30 19:38:21 +0545

    Code refactored

--- src/pages/TableView/index.tsx
+++ src/pages/TableView/index.tsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from "react";
+import { getAllUser } from "../../services/apiServices";
+import { UserDataType } from "../../shared/type";
+import TableRow from "./TableRow";
+
 function TableView() {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(function () {
+    getAllUser().then((data: UserDataType[]) => setUserData(data));
+  }, []);
+
   // Data to be displayed
-  return <div>TableView</div>;
+  return (
+    <>
+      <h1 className="mb-[50px] pl-[50px] text-center text-3xl font-semibold uppercase text-gray-700/80 ">
+        User Data
+      </h1>
+      <table className="m-auto w-4/5 table-fixed border-collapse border-2 border-solid border-gray-300">
+        <thead className="bg-slate-300">
+          <tr className="text-gray-600/70">
+            <th className="">First Name</th>
+            <th>Last Name</th>
+            <th className="">Email</th>
+            <th>Age</th>
+            <th>Address</th>
+            <th>Country</th>
+            <th>City</th>
+            <th>Gender</th>
+            <th>Martial Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody className="">
+          {userData.map((user: UserDataType) => (
+            <TableRow key={user.id} user={user} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default TableView;
--- src/pages/UserForm.tsx/index.tsx
+++ src/pages/UserForm.tsx/index.tsx
@@ -4,6 +4,7 @@ import { UserDataType } from "../../shared/type";
 
 function UserForm() {
   const [data, setData] = useState<UserDataType>({
+    id: "",
     firstName: "",
     lastName: "",
     email: "",
@@ -22,119 +23,132 @@ function UserForm() {
   function handleCreateData(e: React.ChangeEvent<HTMLFormElement>) {
     e.preventDefault();
     createUser(data);
+    // setData({
+    //   id: "",
+    //   firstName: "",
+    //   lastName: "",
+    //   email: "",
+    //   age: 0,
+    //   address: "",
+    //   country: "",
+    //   city: "",
+    //   gender: "",
+    //   martialStatus: "",
+    // });
   }
 
   return (
-    <form
-      className="grid grid-cols-gridAutoFitCol gap-xl p-[50px]"
-      onSubmit={handleCreateData}
-    >
-      <div className="flex flex-col">
-        <label className="ml-1 text-[1rem] font-semibold text-gray-500">
-          First Name
-        </label>
-        <input
-          type="text"
-          placeholder="First name"
-          className="input"
-          name="firstName"
-          value={data.firstName}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="ml-1 text-[0.85rem] text-gray-500">Last Name</label>
-        <input
-          type="text"
-          placeholder="Last name"
-          className="input"
-          name="lastName"
-          value={data.lastName}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="ml-1 text-[0.85rem] text-gray-500">Email</label>
-        <input
-          type="text"
-          placeholder="Email"
-          className="input"
-          name="email"
-          value={data.email}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="ml-1 text-[0.85rem] text-gray-500">Age</label>
-        <input
-          type="text"
-          placeholder="Age"
-          className="input"
-          name="age"
-          value={data.age}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="ml-1 text-[0.85rem] text-gray-500">Address</label>
-        <input
-          type="text"
-          placeholder="Address"
-          className="input"
-          name="address"
-          value={data.address}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="ml-1 text-[0.85rem] text-gray-500">Country</label>
-        <input
-          type="text"
-          placeholder="Country"
-          className="input"
-          name="country"
-          value={data.country}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="ml-1 text-[0.85rem] text-gray-500">City</label>
-        <input
-          type="text"
-          placeholder="City"
-          className="input"
-          name="city"
-          value={data.city}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="ml-1 text-[0.85rem] text-gray-500">Gender</label>
-        <input
-          type="text"
-          placeholder="Gender"
-          className="input"
-          name="gender"
-          value={data.gender}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="flex flex-col">
-        <label className="ml-1 text-[0.85rem] text-gray-500">
-          Martial Status
-        </label>
-        <input
-          type="text"
-          placeholder="Martial status"
-          className="input"
-          name="martialStatus"
-          value={data.martialStatus}
-          onChange={handleInputChange}
-        />
-      </div>
+    <>
+      <h1 className="mb-[50px] pl-[50px] text-center text-3xl font-semibold uppercase text-gray-700/80  ">
+        User form
+      </h1>
+      <form
+        className="grid grid-cols-gridAutoFitCol gap-xl px-[50px]"
+        onSubmit={handleCreateData}
+      >
+        <div className="flex flex-col">
+          <label className=" text-[1rem] text-gray-500">First Name</label>
+          <input
+            type="text"
+            placeholder="First name"
+            className="input"
+            name="firstName"
+            value={data.firstName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className=" text-[1rem] text-gray-500">Last Name</label>
+          <input
+            type="text"
+            placeholder="Last name"
+            className="input"
+            name="lastName"
+            value={data.lastName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className=" text-[1rem] text-gray-500">Email</label>
+          <input
+            type="text"
+            placeholder="Email"
+            className="input"
+            name="email"
+            value={data.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className=" text-[1rem] text-gray-500">Age</label>
+          <input
+            type="text"
+            placeholder="Age"
+            className="input"
+            name="age"
+            value={data.age}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className=" text-[1rem] text-gray-500">Address</label>
+          <input
+            type="text"
+            placeholder="Address"
+            className="input"
+            name="address"
+            value={data.address}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className=" text-[1rem] text-gray-500">Country</label>
+          <input
+            type="text"
+            placeholder="Country"
+            className="input"
+            name="country"
+            value={data.country}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className=" text-[1rem] text-gray-500">City</label>
+          <input
+            type="text"
+            placeholder="City"
+            className="input"
+            name="city"
+            value={data.city}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className=" text-[1rem] text-gray-500">Gender</label>
+          <input
+            type="text"
+            placeholder="Gender"
+            className="input"
+            name="gender"
+            value={data.gender}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className=" text-[1rem] text-gray-500">Martial Status</label>
+          <input
+            type="text"
+            placeholder="Martial status"
+            className="input"
+            name="martialStatus"
+            value={data.martialStatus}
+            onChange={handleInputChange}
+          />
+        </div>
 
-      <button className="bg-blue-400 p-4 text-gray-200">Submit</button>
-    </form>
+        <button className="bg-blue-400 p-4 text-gray-200">Submit</button>
+      </form>
+    </>
   );
 }
 
