d66368c22afe17d3294818a3361d2d81a7fb8a46
milan0827 <milanrokaya2014@gmail.com>
2024-01-31 23:11:40 +0545

    axios instance method created

--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -1,26 +1,40 @@
 import axios from "axios";
-import { UserDataType } from "../shared/type";
 
-const BASE_URL = "http://localhost:3000/user";
-
-const userApi = axios.create({
-  baseURL: BASE_URL,
-  timeout: 2000,
+export const service = axios.create({
+  baseURL: "http://localhost:3000",
+  timeout: 1000,
 });
 
-export async function createUser(data: UserDataType) {
-  await userApi.post("/", { ...data });
-}
+service.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (err: Error) {
+    return Promise.reject(err);
+  },
+);
 
-export async function getAllUser() {
-  const data = await userApi.get("/").then((res) => res.data);
-  return data;
-}
+service.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (err: Error) {
+    return Promise.reject(err);
+  },
+);
+// export async function createUser(data: UserDataType) {
+//   await userApi.post("/", data);
+// }
 
-export async function getUser(id: string) {
-  const data = await userApi.get(`/${id}`);
-  return data;
-}
+// export async function getAllUser() {
+//   const data = await userApi.get("/").then((res) => res.data);
+//   return data;
+// }
+
+// export async function getUser(id: string) {
+//   const data = await userApi.get(`/${id}`);
+//   return data;
+// }
 
 // userApi.interceptors.request.use(
 //   function (config) {
