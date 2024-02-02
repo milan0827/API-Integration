4b372d89f04644dbc172899a440c21393209958b
milan0827 <milanrokaya2014@gmail.com>
2024-02-02 15:06:47 +0545

    file imported with absolute path

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { AxiosError, AxiosResponse } from "axios";
+import { service } from "~/services/apiServices";
+
+export default function useFetch(url: string) {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  async function fetchData() {
+    setIsLoading(true);
+
+    try {
+      const response: AxiosResponse = await service.get(url);
+      setData(response.data);
+    } catch (error) {
+      const err = error as AxiosError;
+      setError(err.message);
+    }
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return { data, isLoading, error };
+}
